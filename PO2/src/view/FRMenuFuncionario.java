2de042dbc826da973bd3f68dd7fb7826cb95d858
Cauasm1 <cauam2498@gmail.com>
2024-04-22 19:34:22 -0300

    caua10

--- PO2/src/view/FRMenuFuncionario.java
+++ PO2/src/view/FRMenuFuncionario.java
@@ -46,7 +46,6 @@ public class FRMenuFuncionario extends javax.swing.JDialog {
         jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         mMenu = new javax.swing.JMenu();
-        miMenuInicial = new javax.swing.JMenuItem();
         miSair = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
         miConsUsuario = new javax.swing.JMenuItem();
@@ -138,19 +137,6 @@ public class FRMenuFuncionario extends javax.swing.JDialog {
 
         mMenu.setText("Menu");
 
-        miMenuInicial.setText("Menu Inicial");
-        miMenuInicial.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                miMenuInicialMouseClicked(evt);
-            }
-        });
-        miMenuInicial.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                miMenuInicialActionPerformed(evt);
-            }
-        });
-        mMenu.add(miMenuInicial);
-
         miSair.setText("Sair");
         miSair.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -223,16 +209,6 @@ public class FRMenuFuncionario extends javax.swing.JDialog {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void miMenuInicialMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_miMenuInicialMouseClicked
-        this.dispose();
-        new FRMenu().setVisible(true);
-    }//GEN-LAST:event_miMenuInicialMouseClicked
-
-    private void miMenuInicialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miMenuInicialActionPerformed
-        this.dispose();
-        new FRMenu().setVisible(true);
-    }//GEN-LAST:event_miMenuInicialActionPerformed
 
     private void miSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miSairActionPerformed
         this.dispose();
@@ -328,7 +304,6 @@ public class FRMenuFuncionario extends javax.swing.JDialog {
     private javax.swing.JMenu mMenu;
     private javax.swing.JMenuItem miCadFuncionario;
     private javax.swing.JMenuItem miConsUsuario;
-    private javax.swing.JMenuItem miMenuInicial;
     private javax.swing.JMenuItem miSair;
     private javax.swing.JMenuItem miSobre;
     // End of variables declaration//GEN-END:variables
